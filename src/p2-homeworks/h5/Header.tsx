2fbf3b45c5aeadd02701eaf1f86e1c2bf1d70f40
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-05-05 20:15:38 +0300

    HW5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {PATH} from "./Routes";
+import {NavLink} from "react-router-dom";
+import s from "./NavLink.module.css"
 
 function Header() {
+    const [openBurger, setOpenBurger] = useState<boolean>(false)
+    const burgerOpen = () => {
+        openBurger ? setOpenBurger(false) : setOpenBurger(true)
+    }
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+            <div className={openBurger ? s.openBurger : s.burger} onClick={burgerOpen}>
+                <span></span>
+            </div>
+            <div className={openBurger ? s.navlink : s.openNavlink}>
+                <NavLink to={PATH.PRE_JUNIOR} className={s.pre_junior} activeClassName={s.active}>pre-junior</NavLink>
+                <NavLink to={PATH.JUNIOR} className={s.junior} activeClassName={s.active}>junior</NavLink>
+                <NavLink to={PATH.JUNIOR_Plus} className={s.junior_plus} activeClassName={s.active}>junior+</NavLink>
+            </div>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export function Junior() {
+    return (
+        <div>
+            {/* <HW7/>
+            <HW8/>
+            <HW9/>
+            <HW10/>
+            <HW11/>*/}
+        </div>
+    )
+}
+
+export function Junior_Plus() {
+    return (
+        <div>
+            {/*<HW12/>*/}
+        </div>
+    )
+}
